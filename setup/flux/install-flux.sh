90047fd4990fb54abbb9bd6e78ac9908d67de145
Matt Thorley <matt.thorley@gmail.com>
2024-01-17 22:04:31 +1100

    Fix metallb configmap

--- setup/flux/install-flux.sh
+++ setup/flux/install-flux.sh
@@ -6,4 +6,8 @@ export REPO_ROOT=$(git rev-parse --show-toplevel)
 
 . "$REPO_ROOT"/.env
 
-flux bootstrap github --owner=mthorley --repository=k8s-gitops --path=clusters/staging --personal --ssh-key-algorithm=ecdsa
+# staging
+# flux bootstrap github --owner=mthorley --repository=k8s-gitops --path=clusters/staging --personal --ssh-key-algorithm=ecdsa
+
+# production
+flux bootstrap github --owner=mthorley --repository=k8s-gitops --path=clusters/production --personal --ssh-key-algorithm=ecdsa

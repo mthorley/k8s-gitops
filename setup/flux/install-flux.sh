7204c7bacb66908d0267c6c2e66412830bc19184
Matt Thorley <matt.thorley@gmail.com>
2024-02-22 13:16:32 +1100

    Flux fixes

--- setup/flux/install-flux.sh
+++ setup/flux/install-flux.sh
@@ -7,7 +7,7 @@ export REPO_ROOT=$(git rev-parse --show-toplevel)
 . "$REPO_ROOT"/.env
 
 # staging
-# flux bootstrap github --owner=mthorley --repository=k8s-gitops --path=clusters/staging --personal --ssh-key-algorithm=ecdsa
+flux bootstrap github --owner=mthorley --repository=k8s-gitops --path=clusters/staging --personal --ssh-key-algorithm=ecdsa
 
 # production
-flux bootstrap github --owner=mthorley --repository=k8s-gitops --path=clusters/production --personal --ssh-key-algorithm=ecdsa
+#flux bootstrap github --owner=mthorley --repository=k8s-gitops --path=clusters/production --personal --ssh-key-algorithm=ecdsa

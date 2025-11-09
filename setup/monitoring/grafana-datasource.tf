6ae538b43297ad939a8de012de9afea550a2ba20
Matt Thorley <matt.thorley@gmail.com>
2025-11-10 06:49:41 +1100

    uid to ref dashboards

--- setup/monitoring/grafana-datasource.tf
+++ setup/monitoring/grafana-datasource.tf
@@ -1,6 +1,7 @@
 resource "grafana_data_source" "influxdb_power" {
   type                = "influxdb"
   name                = "InfluxDB-power"
+  uid                 = "gvJin5OVz"
   url                 = local.influxdb_url
   database_name       = "power" # FIXME: use local vars from influxdb
   json_data_encoded   = jsonencode({})
@@ -9,6 +10,7 @@ resource "grafana_data_source" "influxdb_power" {
 resource "grafana_data_source" "influxdb_rackcontroller" {
   type                = "influxdb"
   name                = "InfluxDB-rackcontroller"
+  uid                 = "MmT8wId4z"
   url                 = local.influxdb_url
   database_name       = "rackcontroller" # FIXME: use local vars from influxdb
   json_data_encoded   = jsonencode({})
@@ -17,6 +19,7 @@ resource "grafana_data_source" "influxdb_rackcontroller" {
 resource "grafana_data_source" "influxdb_tasmota" {
   type                = "influxdb"
   name                = "InfluxDB-Tasmota"
+  uid                 = "ZxLMwSd4z"
   url                 = local.influxdb_url
   database_name       = "tasmota_rssi" # FIXME: use local vars from influxdb
   json_data_encoded   = jsonencode({})
@@ -33,6 +36,7 @@ resource "grafana_data_source" "influxdb_unifitemps" {
 resource "grafana_data_source" "loki" {
   type                = "loki"
   name                = "Loki"
+  uid                 = "2_DjQId4k"
   url                 = local.loki_url
   json_data_encoded   = jsonencode({})
 }
